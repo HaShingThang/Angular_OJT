6a8b38846b22bf69b2521b98cb42293aa3db3254
HaShingThang <scm.hashingthang@gmail.com>
2023-03-03 09:15:07 +0630

    modified formatting code

--- Day03/material_form/src/app/components/input-form/input-form.component.ts
+++ Day03/material_form/src/app/components/input-form/input-form.component.ts
@@ -1,16 +1,23 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
-  selector: 'app-input-form',
-  templateUrl: './input-form.component.html',
-  styleUrls: ['./input-form.component.css']
+	selector: 'app-input-form',
+	templateUrl: './input-form.component.html',
+	styleUrls: ['./input-form.component.css']
 })
 export class InputFormComponent {
-  signin: FormGroup = new FormGroup({
-    email: new FormControl('', [Validators.email, Validators.required]),
-    password: new FormControl('', [Validators.required, Validators.minLength(6)])
-  });
-  hide = true;
-  get emailInput() { return this.signin.get('email'); }
-  get passwordInput() { return this.signin.get('password'); }
+	signin: FormGroup = new FormGroup({
+		email: new FormControl('', [Validators.email, Validators.required]),
+		password: new FormControl('', [Validators.required, Validators.minLength(6)])
+	});
+
+	hide = true;
+
+	get emailInput() {
+		return this.signin.get('email')
+	}
+
+	get passwordInput() {
+		return this.signin.get('password')
+	}
 }

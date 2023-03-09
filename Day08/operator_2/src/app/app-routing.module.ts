0ef45815e732683283542565caec2aebcbb0732e
HaShingThang <scm.hashingthang@gmail.com>
2023-03-09 11:04:57 +0630

    added operators

--- Day08/operator_2/src/app/app-routing.module.ts
+++ Day08/operator_2/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { OperatorsComponent } from './components/operators/operators.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "", component: HomeComponent,
+    children: [
+      { path: "", redirectTo: "operators", pathMatch: "full" },
+      { path: "operators", component: OperatorsComponent }
+    ]
+  },
+  { path: "**", component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Day08/operator_2/src/app/app.module.ts
+++ Day08/operator_2/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { HomeComponent } from './components/home/home.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { OperatorsComponent } from './components/operators/operators.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    NotFoundComponent,
+    HomeComponent,
+    SideBarComponent,
+    OperatorsComponent
   ],
   imports: [
     BrowserModule,

e3bbe6c8fb48eb04bb88fe107f1c2d0b34cc28ff
HaShingThang <scm.hashingthang@gmail.com>
2023-03-10 11:16:41 +0630

    modified formatting code

--- Day09/angular_ojt_project/src/app/components/register/register.component.ts
+++ Day09/angular_ojt_project/src/app/components/register/register.component.ts
@@ -3,37 +3,35 @@ import { FormControl, Validators } from '@angular/forms';
 
 
 interface Role {
-  value: string
+    value: string
 }
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+    selector: 'app-register',
+    templateUrl: './register.component.html',
+    styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
-  roles: Role[] = [
-    { value: "Manager" },
-    { value: "Member" },
-    { value: "User" }
-  ]
-  name = new FormControl('', [Validators.required, Validators.minLength(3)]);
-  email = new FormControl('', [Validators.required, Validators.email]);
-  hide = true;
+    roles: Role[] = [
+        { value: "Manager" },
+        { value: "Member" },
+        { value: "User" }
+    ]
+    name = new FormControl('', [Validators.required, Validators.minLength(3)]);
+    email = new FormControl('', [Validators.required, Validators.email]);
+    hide = true;
 
-  getNameError() {
-    if (this.name.hasError('required')) {
-      return 'You must enter a name';
-    }
-    return this.name.hasError('minLength') ? 'Name should be more than 3 characters' : '';
-  }
-
-
-  getEmailError() {
-    if (this.email.hasError('required')) {
-      return 'Enter your email';
+    getNameError() {
+        if (this.name.hasError('required')) {
+            return 'You must enter a name';
+        }
+        return this.name.hasError('minLength') ? 'Name should be more than 3 characters' : '';
     }
 
-    return this.email.hasError('email') ? 'Email is not valid.' : '';
-  }
+    getEmailError() {
+        if (this.email.hasError('required')) {
+            return 'Enter your email';
+        }
+        return this.email.hasError('email') ? 'Email is not valid.' : '';
+    }
 }

d3cd73f4c2844d2932574f9eff82aff2961d1a7d
HaShingThang <scm.hashingthang@gmail.com>
2023-03-10 14:16:45 +0630

    modified formatting code

--- Day09/angular_ojt_project/src/app/components/login/login.component.ts
+++ Day09/angular_ojt_project/src/app/components/login/login.component.ts
@@ -4,30 +4,34 @@ import { Router } from '@angular/router';
 @Component({
     selector: 'app-login',
     templateUrl: './login.component.html',
-    styleUrls: ['./login.component.scss']
+    styleUrls: ['./login.component.scss'],
 })
-
 export class LoginComponent {
     loginForm: FormGroup | any;
-     hide: boolean = true
-    constructor(
-        private router: Router
-    ) {
+    hide: boolean = true;
+
+    constructor(private router: Router) {
         this.loginForm = new FormGroup({
-            email: new FormControl('', [Validators.required, Validators.email, Validators.pattern(
-                '[a-z0-9]+@[a-z]+\.[a-z]{2,3}',
-            ),]),
-            password: new FormControl('', [Validators.required, Validators.pattern(
-                '^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{5,8}$'
-            )])
+            email: new FormControl('', [
+                Validators.required,
+                Validators.email,
+                Validators.pattern('[a-z0-9]+@[a-z]+.[a-z]{2,3}'),
+            ]),
+            password: new FormControl('', [
+                Validators.required,
+                Validators.pattern(
+                    '^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{5,8}$'
+                ),
+            ]),
         });
     }
-    ngOnInit(): void {
-    }
+
+    ngOnInit(): void { }
+
     onSubmit() {
         if (!this.loginForm.valid) {
             return;
         }
-        localStorage.setItem('user', JSON.stringify(this.loginForm.value))
+        localStorage.setItem('user', JSON.stringify(this.loginForm.value));
     }
 }
